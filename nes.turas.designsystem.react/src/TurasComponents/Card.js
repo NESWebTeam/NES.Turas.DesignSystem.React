d4e872faa98b6ffb47d1ecdca1c1ba0701772053
markwdownie <mark.downie@nes.scot.nhs.uk>
2024-05-30 11:59:55 +0100

    Card js first component

---
+++ nes.turas.designsystem.react/src/TurasComponents/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = ({cardHeading, cardLink}) => {
+    return (
+      <div class="col-md-6 col-lg-4  "><a href={cardLink}>
+        <div class="card landingBlock h-100  ">
+          <div class="card-heading">
+            <h3 class="card-title">{cardHeading}</h3>
+          </div>
+          <div class="card-body d-flex flex-column">
+            <p>lorem</p>
+          </div>
+        </div>
+      </a></div>
+    )
+  }
+  
+  export default Card
